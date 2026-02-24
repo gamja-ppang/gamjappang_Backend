160c7300d4f801a0838300d219249f9e7c5b2bd3
tlgms <siheun9@gmail.com>
2026-02-24 16:48:21 +0900

    fix: @EnableAsync 빈 등록(#4)

--- src/main/java/demago/gamjappang/GamjaPpangApplication.java
+++ src/main/java/demago/gamjappang/GamjaPpangApplication.java
@@ -3,9 +3,11 @@ package demago.gamjappang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableAsync
 public class GamjaPpangApplication {
 
     public static void main(String[] args) {

a123c3363ac8a822f9a2a8c6891a2659c2e73a3d
tlgms <siheun9@gmail.com>
2026-03-14 19:49:31 +0900

    feat: 글 저장(CreatePostResult save) 구현(#21)

--- src/main/java/demago/gamjappang/post/infrastructure/adapter/out/persistence/PostPersistenceAdapter.java
+++ src/main/java/demago/gamjappang/post/infrastructure/adapter/out/persistence/PostPersistenceAdapter.java
@@ -1,9 +1,12 @@
 package demago.gamjappang.post.infrastructure.adapter.out.persistence;
 
+import demago.gamjappang.post.applicationcore.port.in.result.CreatePostResult;
 import demago.gamjappang.post.applicationcore.port.out.PostRepositoryPort;
 import demago.gamjappang.post.domain.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PostPersistenceAdapter implements PostRepositoryPort {
 
     private final SpringDataPostJpaRepository repository;
@@ -15,7 +18,17 @@ public class PostPersistenceAdapter implements PostRepositoryPort {
     }
 
     @Override
-    public Post save(Post post) {
-        return mapper.toDomain(repository.save(mapper.toEntity(post)));
+    public CreatePostResult save(Post post) {
+        repository.save(mapper.toEntity(post));
+
+        return new CreatePostResult(
+                post.getId(),
+                new CreatePostResult.Author(post.getUser().getId(), post.getUser().getUsername()),
+                post.getTitle(),
+                post.getContent(),
+                post.getTags(),
+                post.getCreatedAt(),
+                post.getUpdatedAt()
+                );
     }
 }

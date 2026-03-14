ab416e1e4ad424b8c06b675241554b16f00199b6
tlgms <siheun9@gmail.com>
2026-03-14 19:48:07 +0900

    feat: FK 판별용 userId 추가(#21)

--- src/main/java/demago/gamjappang/post/applicationcore/port/in/command/CreatePostCommand.java
+++ src/main/java/demago/gamjappang/post/applicationcore/port/in/command/CreatePostCommand.java
@@ -2,5 +2,7 @@ package demago.gamjappang.post.applicationcore.port.in.command;
 
 import java.util.List;
 
-public record CreatePostCommand(String title, String content, List<String> tags) {
+public record CreatePostCommand(
+        String title, String content, List<String> tags, Long userId
+) {
 }

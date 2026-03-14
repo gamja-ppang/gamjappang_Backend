5ba4ca305f623d48519059be04c311951c7c2f78
tlgms <siheun9@gmail.com>
2026-03-14 19:48:52 +0900

    fix: Result가 아닌 model을 반환하던 문제 수정(#21)

--- src/main/java/demago/gamjappang/post/applicationcore/port/out/PostRepositoryPort.java
+++ src/main/java/demago/gamjappang/post/applicationcore/port/out/PostRepositoryPort.java
@@ -1,7 +1,8 @@
 package demago.gamjappang.post.applicationcore.port.out;
 
+import demago.gamjappang.post.applicationcore.port.in.result.CreatePostResult;
 import demago.gamjappang.post.domain.model.Post;
 
 public interface PostRepositoryPort {
-    Post save(Post post);
+    CreatePostResult save(Post post);
 }

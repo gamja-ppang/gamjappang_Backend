56f9de9ff1f79a7cf302c71f48d1994121da74d5
tlgms <siheun9@gmail.com>
2026-03-17 14:15:09 +0900

    chore: 가독성 수정(#21)

--- src/main/java/demago/gamjappang/post/applicationcore/port/in/command/CreatePostCommand.java
+++ src/main/java/demago/gamjappang/post/applicationcore/port/in/command/CreatePostCommand.java
@@ -3,6 +3,9 @@ package demago.gamjappang.post.applicationcore.port.in.command;
 import java.util.List;
 
 public record CreatePostCommand(
-        String title, String content, List<String> tags, Long userId
+        String title,
+        String content,
+        List<String> tags,
+        Long userId
 ) {
 }

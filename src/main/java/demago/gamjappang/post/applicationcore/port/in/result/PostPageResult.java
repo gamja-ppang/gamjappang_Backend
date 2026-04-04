25d429b488fdda1a9936a9f6a01d9f506d95596f
tlgms <siheun9@gmail.com>
2026-04-05 02:49:29 +0900

    feat: PostPageResult.java 추가(#21)

---
+++ src/main/java/demago/gamjappang/post/applicationcore/port/in/result/PostPageResult.java
@@ -0,0 +1,29 @@
+가package demago.gamjappang.post.applicationcore.port.in.result;
+
+import java.util.List;
+
+public record PostPageResult(
+        List<PostSummary> content,
+        int page,
+        int size,
+        long totalElements,
+        int totalPages,
+        boolean hasNext
+) {
+    public record PostSummary(
+            Long id,
+            String title,
+            Author author,
+            String summation,
+            List<String> tags,
+            int viewCount,
+            int likeCount,
+            int commentCount
+    ) {
+        public record Author(
+                Long id,
+                String name
+        ) {
+        }
+    }
+}

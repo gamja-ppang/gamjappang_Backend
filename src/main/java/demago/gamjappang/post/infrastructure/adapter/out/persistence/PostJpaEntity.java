d5fdec32d8cbce4d740fa5764bd98f547092002f
tlgms <siheun9@gmail.com>
2026-04-07 11:22:34 +0900

    fix: 데이터 타입을 `text`로 변경(#21)

--- src/main/java/demago/gamjappang/post/infrastructure/adapter/out/persistence/PostJpaEntity.java
+++ src/main/java/demago/gamjappang/post/infrastructure/adapter/out/persistence/PostJpaEntity.java
@@ -22,6 +22,7 @@ public class PostJpaEntity {
     @Column(nullable = false)
     private String title;
 
+    @Lob
     @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 

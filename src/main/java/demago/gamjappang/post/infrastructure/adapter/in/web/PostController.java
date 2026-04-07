5523e8f461c89e7ba3853a1bed24305371377caa
tlgms <siheun9@gmail.com>
2026-04-07 14:42:16 +0900

    feat: `Get /api/v1/post` 글 페이징 리스트 엔드포인트 추가(#21)

--- src/main/java/demago/gamjappang/post/infrastructure/adapter/in/web/PostController.java
+++ src/main/java/demago/gamjappang/post/infrastructure/adapter/in/web/PostController.java
@@ -2,17 +2,23 @@ package demago.gamjappang.post.infrastructure.adapter.in.web;
 
 import demago.gamjappang.global.security.userdetails.UserPrincipal;
 import demago.gamjappang.post.applicationcore.port.in.CreatePostUseCase;
+import demago.gamjappang.post.applicationcore.port.in.PostPageUseCase;
 import demago.gamjappang.post.applicationcore.port.in.UpdatePostUseCase;
 import demago.gamjappang.post.applicationcore.port.in.command.CreatePostCommand;
+import demago.gamjappang.post.applicationcore.port.in.command.PostPageCommand;
 import demago.gamjappang.post.applicationcore.port.in.command.UpdatePostCommand;
 import demago.gamjappang.post.applicationcore.port.in.result.CreatePostResult;
+import demago.gamjappang.post.applicationcore.port.in.result.PostPageResult;
 import demago.gamjappang.post.applicationcore.port.in.result.UpdatePostResult;
 import demago.gamjappang.post.domain.model.Post;
 import demago.gamjappang.post.infrastructure.adapter.in.web.dto.request.CreatePostRequest;
 import demago.gamjappang.post.infrastructure.adapter.in.web.dto.request.UpdatePostRequset;
 import demago.gamjappang.post.infrastructure.adapter.in.web.dto.response.CreatePostResponse;
+import demago.gamjappang.post.infrastructure.adapter.in.web.dto.response.PostPageResponse;
 import demago.gamjappang.post.infrastructure.adapter.in.web.dto.response.UpdatePostResponse;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -23,12 +29,15 @@ public class PostController {
 
     private final CreatePostUseCase createPostUseCase;
     private final UpdatePostUseCase updatePostUseCase;
+    private final PostPageUseCase postPageUseCase;
 
     public PostController(
             CreatePostUseCase createPostUseCase,
-            UpdatePostUseCase updatePostUseCase) {
+            UpdatePostUseCase updatePostUseCase,
+            PostPageUseCase postPageUseCase) {
         this.createPostUseCase = createPostUseCase;
         this.updatePostUseCase = updatePostUseCase;
+        this.postPageUseCase = postPageUseCase;
     }
 
     @PostMapping
@@ -45,4 +54,13 @@ public class PostController {
         return ResponseEntity.ok(UpdatePostResponse.from(result));
     }
 
+    @GetMapping("/post")
+    public ResponseEntity<PostPageResponse> getPostList(@RequestParam(required = false) String keyword,
+                                                        @RequestParam(required = false) String tag,
+                                                        Pageable pageable) {
+        PostPageCommand command = new PostPageCommand(keyword, tag, pageable);
+        PostPageResult result = postPageUseCase.getPostPage(command);
+
+        return ResponseEntity.ok(PostPageResponse.from(result));
+    }
 }

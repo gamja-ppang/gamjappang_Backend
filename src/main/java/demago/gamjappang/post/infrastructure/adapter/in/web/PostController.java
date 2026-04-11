f515029c944dbea7d3b1618419bd0d9ef68e6195
tlgms <siheun9@gmail.com>
2026-04-11 19:46:28 +0900

    feat: `Get /api/v1/post/{postId}` 게시글 상세 정보 조회 엔드포인트 추가(#21)

--- src/main/java/demago/gamjappang/post/infrastructure/adapter/in/web/PostController.java
+++ src/main/java/demago/gamjappang/post/infrastructure/adapter/in/web/PostController.java
@@ -2,18 +2,22 @@ package demago.gamjappang.post.infrastructure.adapter.in.web;
 
 import demago.gamjappang.global.security.userdetails.UserPrincipal;
 import demago.gamjappang.post.applicationcore.port.in.CreatePostUseCase;
+import demago.gamjappang.post.applicationcore.port.in.GetPostUseCase;
 import demago.gamjappang.post.applicationcore.port.in.PostPageUseCase;
 import demago.gamjappang.post.applicationcore.port.in.UpdatePostUseCase;
 import demago.gamjappang.post.applicationcore.port.in.command.CreatePostCommand;
+import demago.gamjappang.post.applicationcore.port.in.command.GetPostCommand;
 import demago.gamjappang.post.applicationcore.port.in.command.PostPageCommand;
 import demago.gamjappang.post.applicationcore.port.in.command.UpdatePostCommand;
 import demago.gamjappang.post.applicationcore.port.in.result.CreatePostResult;
+import demago.gamjappang.post.applicationcore.port.in.result.GetPostResult;
 import demago.gamjappang.post.applicationcore.port.in.result.PostPageResult;
 import demago.gamjappang.post.applicationcore.port.in.result.UpdatePostResult;
 import demago.gamjappang.post.domain.model.Post;
 import demago.gamjappang.post.infrastructure.adapter.in.web.dto.request.CreatePostRequest;
 import demago.gamjappang.post.infrastructure.adapter.in.web.dto.request.UpdatePostRequset;
 import demago.gamjappang.post.infrastructure.adapter.in.web.dto.response.CreatePostResponse;
+import demago.gamjappang.post.infrastructure.adapter.in.web.dto.response.GetPostResponse;
 import demago.gamjappang.post.infrastructure.adapter.in.web.dto.response.PostPageResponse;
 import demago.gamjappang.post.infrastructure.adapter.in.web.dto.response.UpdatePostResponse;
 import jakarta.validation.Valid;
@@ -30,37 +34,57 @@ public class PostController {
     private final CreatePostUseCase createPostUseCase;
     private final UpdatePostUseCase updatePostUseCase;
     private final PostPageUseCase postPageUseCase;
+    private final GetPostUseCase getPostUseCase;
 
     public PostController(
             CreatePostUseCase createPostUseCase,
             UpdatePostUseCase updatePostUseCase,
-            PostPageUseCase postPageUseCase) {
+            PostPageUseCase postPageUseCase,
+            GetPostUseCase getPostUseCase) {
         this.createPostUseCase = createPostUseCase;
         this.updatePostUseCase = updatePostUseCase;
         this.postPageUseCase = postPageUseCase;
+        this.getPostUseCase = getPostUseCase;
     }
 
     @PostMapping
-    public ResponseEntity<CreatePostResponse> savePost(@Valid @RequestBody CreatePostRequest request, @AuthenticationPrincipal UserPrincipal user) {
+    public ResponseEntity<CreatePostResponse> savePost(
+            @Valid @RequestBody CreatePostRequest request,
+            @AuthenticationPrincipal UserPrincipal user
+    ) {
         CreatePostCommand command = request.toCreatePostCommand(user.getId());
         CreatePostResult result = createPostUseCase.createPost(command);
         return ResponseEntity.ok(CreatePostResponse.from(result));
     }
 
     @PatchMapping("/{postId}")
-    public ResponseEntity<UpdatePostResponse> updatePost(@Valid @RequestBody UpdatePostRequset requset, @AuthenticationPrincipal UserPrincipal user, @PathVariable Long postId) {
+    public ResponseEntity<UpdatePostResponse> updatePost(
+            @Valid @RequestBody UpdatePostRequset requset,
+            @AuthenticationPrincipal UserPrincipal user,
+            @PathVariable Long postId
+    ) {
         UpdatePostCommand command = requset.toUpdateCommand(user.getId(), postId);
         UpdatePostResult result = updatePostUseCase.updatePost(command);
         return ResponseEntity.ok(UpdatePostResponse.from(result));
     }
 
-    @GetMapping("/post")
-    public ResponseEntity<PostPageResponse> getPostList(@RequestParam(required = false) String keyword,
-                                                        @RequestParam(required = false) String tag,
-                                                        Pageable pageable) {
+    @GetMapping()
+    public ResponseEntity<PostPageResponse> getPostList(
+            @RequestParam(required = false) String keyword,
+            @RequestParam(required = false) String tag,
+            Pageable pageable
+    ) {
         PostPageCommand command = new PostPageCommand(keyword, tag, pageable);
         PostPageResult result = postPageUseCase.getPostPage(command);
 
         return ResponseEntity.ok(PostPageResponse.from(result));
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<GetPostResponse> getPost(@PathVariable Long postId) {
+        GetPostCommand command = new GetPostCommand(postId);
+        GetPostResult result = getPostUseCase.getPost(command);
+
+        return ResponseEntity.ok(GetPostResponse.from(result));
     }
 }

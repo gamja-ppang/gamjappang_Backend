6eb1f16c05f37a264783306fe5f6442a037813d8
tlgms <siheun9@gmail.com>
2026-04-14 15:18:57 +0900

    feat: CommentController.java 추가(#26)

--- src/main/java/demago/gamjappang/domain/comment/infrastructure/adepter/in/web/CommentController.java
+++ src/main/java/demago/gamjappang/domain/comment/infrastructure/adepter/in/web/CommentController.java
@@ -1,4 +1,18 @@
 package demago.gamjappang.domain.comment.infrastructure.adepter.in.web;
 
-public interface CommentController {
+import demago.gamjappang.domain.comment.applicationcore.port.in.CreateCommentUseCase;
+import demago.gamjappang.domain.comment.applicationcore.port.in.command.CreateCommentCommand;
+import demago.gamjappang.domain.comment.applicationcore.service.CommentApplicationService;
+import demago.gamjappang.domain.comment.domain.model.Comment;
+import demago.gamjappang.global.security.userdetails.UserPrincipal;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/vi/comment")
+public class CommentController {
+
 }

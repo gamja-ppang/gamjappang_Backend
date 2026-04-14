012f4217ab7b9ba05c21ca1863dc4b22935c2407
tlgms <siheun9@gmail.com>
2026-04-14 14:36:01 +0900

    feat: CommentMapper.java 추가(#26)

---
+++ src/main/java/demago/gamjappang/domain/comment/infrastructure/adepter/out/persistence/CommentMapper.java
@@ -0,0 +1,46 @@
+package demago.gamjappang.domain.comment.infrastructure.adepter.out.persistence;
+
+import demago.gamjappang.domain.comment.domain.model.Comment;
+import demago.gamjappang.domain.post.domain.model.Post;
+import demago.gamjappang.domain.post.infrastructure.adapter.out.persistence.PostJpaEntity;
+import demago.gamjappang.domain.post.infrastructure.adapter.out.persistence.PostMapper;
+import demago.gamjappang.domain.user.domain.model.User;
+import demago.gamjappang.domain.user.infrastructure.adapter.out.persistence.UserJpaEntity;
+import demago.gamjappang.domain.user.infrastructure.adapter.out.persistence.UserMapper;
+
+public class CommentMapper {
+
+    private final PostMapper postMapper;
+    private final UserMapper userMapper;
+
+    public CommentMapper(PostMapper postMapper, UserMapper userMapper) {
+        this.postMapper = postMapper;
+        this.userMapper = userMapper;
+    }
+
+    public Comment toDomain(CommentJpaEntity entity) {
+        Post post = postMapper.toDomain(entity.getPost());
+        User user = userMapper.toDomain(entity.getUser());
+
+        return new Comment(
+                entity.getId(),
+                post,
+                user,
+                entity.getContent(),
+                entity.getCreatedAt()
+        );
+    }
+
+    public CommentJpaEntity toEntity(Comment comment) {
+        PostJpaEntity postEntity = postMapper.toEntity(comment.getPost());
+        UserJpaEntity userEntity = userMapper.toEntity(comment.getUser());
+
+        return new CommentJpaEntity(
+                comment.getId(),
+                postEntity,
+                userEntity,
+                comment.getContent(),
+                comment.getCreatedAt()
+        );
+    }
+}

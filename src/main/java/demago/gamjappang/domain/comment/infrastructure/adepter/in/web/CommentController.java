406d772052deea42cafb2ed5e0427170c4d91e60
tlgms <siheun9@gmail.com>
2026-04-17 13:27:05 +0900

    feat: `Post /api/vi/comment` 댓글 생성 엔드포인트 추가(#26)

--- src/main/java/demago/gamjappang/domain/comment/infrastructure/adepter/in/web/CommentController.java
+++ src/main/java/demago/gamjappang/domain/comment/infrastructure/adepter/in/web/CommentController.java
@@ -4,10 +4,14 @@ import demago.gamjappang.domain.comment.applicationcore.port.in.CreateCommentUse
 import demago.gamjappang.domain.comment.applicationcore.port.in.command.CreateCommentCommand;
 import demago.gamjappang.domain.comment.applicationcore.service.CommentApplicationService;
 import demago.gamjappang.domain.comment.domain.model.Comment;
+import demago.gamjappang.domain.comment.infrastructure.adepter.in.web.dto.request.CreateCommentRequest;
 import demago.gamjappang.global.security.userdetails.UserPrincipal;
+import feign.Body;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/vi/comment")
 public class CommentController {
 
+    private final CreateCommentUseCase createCommentUseCase;
+
+    public CommentController(CreateCommentUseCase createCommentUseCase) {
+        this.createCommentUseCase = createCommentUseCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createComment(
+            @Valid @RequestBody CreateCommentRequest request,
+            @AuthenticationPrincipal UserPrincipal user
+    ) {
+        CreateCommentCommand command = request.toCreateCommentCommand(user.getId());
+
+        createCommentUseCase.createComment(command);
+
+        return ResponseEntity.ok().build();
+    }
 }
